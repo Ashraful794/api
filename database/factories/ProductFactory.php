2bf86d9c186a2c841e486880c1f7292350666071
unknown <ashraful01934207337@gmail.com>
2020-06-09 12:55:15 +0600

    seeding part for product and review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->word,
+        'detail'=>$faker->paragraph,
+        'detail'=>$faker->paragraph,
+        'price'=>$faker->numberbetween(100,1000),
+        'stock'=>$faker->randomDigit,
+        'discount'=>$faker->numberbetween(2,30),
     ];
 });
